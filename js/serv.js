7b8d67818ae65a92e7fce3037225d9e4d2dbba15
Mariano <marianotrogo@gmail.com>
2022-10-25 22:35:40 -0300

    agregadosaltecera

---
+++ js/serv.js
@@ -0,0 +1,33 @@
+const listServ = [
+    {
+      id: 1,
+      nombre: "Netflix",
+      precio: 1000,
+      imagen: "../img/netflix.png"
+      
+    },
+    {
+      id: 2,
+      nombre: "Prime Video",
+      precio: 300,
+      imagen: ".../img/primeVideo.jfif"
+    },
+    {
+      id: 3,
+      nombre: "Hbo Go",
+      precio: 500,
+      imagen:"../img/hboGo.png"
+    },
+    {
+      id: 4,
+      nombre: "Psn Now",
+      precio: 2000,
+      imagen: "../img/psnNow.png"
+    },
+    {
+      id: 5,
+      nombre: "YouTube Premium",
+      precio: 250,
+      imagen: "../img/youtube.jfif"
+    },
+]
